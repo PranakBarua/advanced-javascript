3bb5318bc08c50fba0068280f49aed0388846520
pranak <pranakbarua111@gmail.com>
2020-03-03 23:39:49 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// const age=0;
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+
+//false condition
+//
+//0
+//false
+//""
+//null
+//undefined
+//NaN
+// const name="";
+let name=NaN;
+console.log(name);
+if(name){
+        console.log("condition is true");
+    }
+else{
+        console.log("condition is false");
+    }
